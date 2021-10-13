96ac45a0f0cb9e2ea022dce16059bf66480eb32f
nodejshobby@gmail.com <nodejshobby@gmail.com>
2021-10-13 07:51:41 +0100

    Changes redis connection from localhost to live connection string

--- database/redisConnect.js
+++ database/redisConnect.js
@@ -1,5 +1,7 @@
 const redis=require('redis');
-const client=redis.createClient();
+const client=redis.createClient({
+    url: process.env.REDIS_URL
+});
 
 client.on('error',(error)=>{
     console.log(error.message)
